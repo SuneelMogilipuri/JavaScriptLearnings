94fd1445e6610b077c49812bc5e026c55c520ad9
Mogilipuri Suneel <suneel71421@gmail.com>
2020-04-23 23:42:59 +0530

    Day_2

--- test_for_a_tag_inside_li/edit.js
+++ test_for_a_tag_inside_li/edit.js
@@ -1,24 +1,35 @@
-var loadItems = function loadItems(){
-  var list = [
-    'C',
-    'Java',
-    'php',
-    'C#',
-    'ADO.net',
-    'Entity frameWork',
-    'ROR'
-  ]
-  list.forEach(myFunction); 
-  function myFunction(item, index) 
-  { 
-    var node = document.createElement("LI"); 
-    var textnode = document.createTextNode(item); 
-    node.appendChild(textnode);
-    var image = document.createElement('img');
-    image.src = 'test.png';
-    image.style.height = '50px';
-    image.style.width = '50px';
-    node.appendChild(image);
-    document.getElementById("my-learnings").appendChild(node);
-  }
+function createListItem(text){
+  var li = document.createElement('li');
+  li.style = "list-style:none";
+  li.textContent = text;
+  var image = document.createElement('img');
+  image.src = 'test.png';
+  image.style.height = '50px';
+  image.style.width = '50px';
+  image.setAttribute('onclick','imgClick(this)');
+  image.style.cursor = "pointer";
+  li.appendChild(image);
+  return li;
 }
+
+var list = [
+  createListItem('C'),
+  createListItem('Java'),
+  createListItem('php'),
+  createListItem('C#'),
+  createListItem('ADO.net'),
+  createListItem('Entity frameWork'),
+  createListItem('ROR')
+]
+
+function appendChildren(parent, children){
+  children.forEach(function (child) {
+    parent.appendChild(child);
+  });
+}
+function imgClick(ele){
+  document.getElementById('selected-lang').value = ele.parentElement.textContent;
+  // console.log(ele);
+}
+var myLearning = document.getElementById('my-learnings');
+appendChildren(myLearning, list);
